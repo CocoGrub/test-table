8d9812d6e3518186e5062c596df91446b0a95466
CocoGrub <fedsavin@gmail.com>
2020-08-21 12:45:47 +0300

    mapStateToProps

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,30 +1,41 @@
 import React from "react";
-import {createPortal} from "react-dom";
-import {connect} from "react-redux";
-import {getUsers, maxSource, minSource} from "../store/MainReducer";
-import PropTypes from 'prop-types'
+import { createPortal } from "react-dom";
+import { connect } from "react-redux";
+import { getUsers, maxSource, minSource } from "../store/MainReducer";
+import PropTypes from "prop-types";
 
-const Modal = ({modal,getUsers}) => {
+const Modal = ({ modal, getUsers }) => {
+  if (modal) {
+    return createPortal(
+      <div className={"modal"}>
+        <div className={"center"}>
+          <button
+            className={"modal-button"}
+            onClick={() => getUsers(minSource)}
+          >
+            Display little table
+          </button>
+          <button
+            className={"modal-button"}
+            onClick={() => getUsers(maxSource)}
+          >
+            Display BIG table
+          </button>
+        </div>
+      </div>,
+      document.body
+    );
+  }
+  return null;
+};
 
-    if (modal) {
-        return createPortal(<div className={'modal'}>
-            <div className={"center"}>
-                <button className={"modal-button"}  onClick={()=>getUsers(minSource)}>Display little table</button>
-                <button className={"modal-button"}  onClick={()=>getUsers(maxSource)}>Display BIG table</button>
-            </div>
-        </div>, document.body)
-    }
-    return null
-}
+Modal.propTypes = {
+  modal: PropTypes.bool.isRequired,
+  getUsers: PropTypes.func.isRequired,
+};
 
-Modal.propTypes={
-    modal:PropTypes.bool.isRequired,
-    getUsers:PropTypes.func.isRequired
-}
+const mapStateToProps = (state) => ({
+  modal: state.modal,
+});
 
-const mapDispatchToProps=(state)=>({
-    modal:state.modal
-})
-
-
-export default connect(mapDispatchToProps,{getUsers})(Modal);
+export default connect(mapStateToProps, { getUsers })(Modal);
