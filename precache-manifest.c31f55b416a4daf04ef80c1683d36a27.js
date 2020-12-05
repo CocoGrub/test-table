f3c8411c177df33d7225f4fbc4543256e7a8f2fc
CocoGrub <fedsavin@gmail.com>
2020-12-05 07:52:57 +0300

    Updates

---
+++ precache-manifest.c31f55b416a4daf04ef80c1683d36a27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93da6d80d2f911491cdc6e34a8874fd",
+    "url": "/ShopWithCart/index.html"
+  },
+  {
+    "revision": "7ff4a7332eb9814a566a",
+    "url": "/ShopWithCart/static/css/main.15611f0a.chunk.css"
+  },
+  {
+    "revision": "8384f4d4be90399ce8f0",
+    "url": "/ShopWithCart/static/js/2.0308edd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ShopWithCart/static/js/2.0308edd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff4a7332eb9814a566a",
+    "url": "/ShopWithCart/static/js/main.68143558.chunk.js"
+  },
+  {
+    "revision": "2c931351e30eaca1a968",
+    "url": "/ShopWithCart/static/js/runtime-main.9f2588dd.js"
+  },
+  {
+    "revision": "a80c5d39901d430c39e951a9d8fd70a4",
+    "url": "/ShopWithCart/static/media/Ripple-1s-200px.a80c5d39.svg"
+  }
+]);
